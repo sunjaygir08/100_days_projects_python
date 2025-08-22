250bb5677b6333811c3e30fc18c97a6158f7dead
Sunjay Gir <sunjaygir@gmail.com>
2025-08-22 14:45:55 +0500

    changing

--- Banking Application.py
+++ Banking Application.py
@@ -1,6 +1,6 @@
 class Account:
     def __init__(self):
-        self.account = 0
+        self.balance = 0
         print("New Account is Created")
 
     def deposit(self):
@@ -9,7 +9,7 @@ class Account:
         print("New balance is:", self.balance)
 
     def withdraw(self):
-        amount=float(input("Enter the amount to withdraw: "))
+        amount = float(input("Enter the amount to withdraw: "))
         if (amount > self.balance):
             print("Insufficient balance")
         else:
@@ -19,7 +19,7 @@ class Account:
     def inquiry(self):
         print("Current balance is:", self.balance)
 
-ac=Account()
+ac = Account()
 ac.deposit()
 ac.withdraw()
 ac.inquiry()
